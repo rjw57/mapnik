6e2474e3fcb9933dfaa3e3f287f5f1433dfc3cfd
Rich Wareham <rjw57@cam.ac.uk>
2012-07-31 16:38:57 +0100

    move examples

--- plugins/input/python/examples/simple_xml.py
+++ plugins/input/python/examples/simple_xml.py
@@ -1,6 +1,6 @@
 import mapnik
-stylesheet = 'test.xml'
-image = 'test.png'
+stylesheet = 'simple_xml.xml'
+image = 'simple_xml.png'
 m = mapnik.Map(600, 300)
 mapnik.load_map(m, stylesheet)
 m.zoom_all() 
